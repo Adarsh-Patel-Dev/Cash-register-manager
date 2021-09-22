57131acb87b61dc649ed7291d877e453aeea8352
Adarsh2k21 <adarsh.patel9619@gmail.com>
2021-09-23 00:06:23 +0530

    fix: negative input and modified ui

--- index.js
+++ index.js
@@ -2,35 +2,63 @@ const billAmount = document.querySelector("#bill-amount");
 const cashGiven = document.querySelector("#cash-given");
 const message = document.querySelector("#error-message");
 
+var cashGivenDiv = document.querySelector(".cashGivenInput");
+var changeReturnDiv = document.querySelector(".changeReturn");
 
-
+var nextButton = document.querySelector("#next");
 
 const checkButton = document.querySelector("#check-button");
 const noOfNotes = document.querySelectorAll(".no-of-notes");
 
 const availableNotes = [2000, 500, 100, 20, 10, 5, 1];
 
+nextButton.addEventListener("click", ()=> {
+    if(billAmount.value){
+         hideMessage();
+         
+        if(Number(billAmount.value)>0){
+            nextButton.style.display = "none";
+            cashGivenDiv.style.display = "block";
+        }
+        else{
+            showMessage("Enter valid bill amount 🙄");
+        }
+    } 
+    // showMessage("Please enter  valid bill amount");
+})
+
 checkButton.addEventListener("click", function validateBillAndCashAmount() {
 
     hideMessage();
 
     if (billAmount.value > 0) {
-        if (Number(cashGiven.value) >= Number(billAmount.value)) {
+        if (Number(cashGiven.value) > Number(billAmount.value)) {
             const amountToBeReturned = Number(cashGiven.value) - Number(billAmount.value);
             console.log(amountToBeReturned);
             calculateChange(amountToBeReturned);
+
+            changeReturnDiv.style.display = "block";
+            // checkButton.style.display = "none";
+
+            billAmount.value = " ";
+            cashGiven.value = " ";
+
         } else if (cashGiven.value === "" ) {
             showMessage("Please enter amount of cash");
-         } 
+         } else if(cashGiven.value == billAmount.value){
+             showMessage("No amount should be returned");
+         }
+         else if(cashGiven.value < 0){
+            showMessage("Cash amount can not be negative🤨. Please enter valid value.");
+           
+         }
 
         else {
             showMessage(
                 "The cash provided should be equal or more to the bill amount");
              }
-    } else {
-        showMessage("Please enter both the values.");
-       
-    }
+    } 
+     
 });
 
 function calculateChange(amountToBeReturned) {
